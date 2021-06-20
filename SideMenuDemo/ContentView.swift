8b052f6cbe5794e53e16e7f029fb02b0924ae3cf
Frederick Javalera <frederickjavalera@gmail.com>
2021-06-20 13:44:45 -0600

    Initial Commit

---
+++ SideMenuDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SideMenuDemo
+//
+//  Created by Frederick Javalera on 6/20/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SideMenuDemo/SideMenuDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SideMenuDemoApp.swift
+//  SideMenuDemo
+//
+//  Created by Frederick Javalera on 6/20/21.
+//
+
+import SwiftUI
+
+@main
+struct SideMenuDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
