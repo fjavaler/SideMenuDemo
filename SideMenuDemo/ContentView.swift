9c6304f78602efa08ba95956bf6acc99a1d6c845
Frederick Javalera <frederickjavalera@gmail.com>
2021-06-20 14:09:08 -0600

    - Finished.

--- SideMenuDemo/ContentView.swift
+++ SideMenuDemo/ContentView.swift
@@ -8,14 +8,60 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isDrawerOpen: Bool = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            /// Navigation Bar Title part
+            if !self.isDrawerOpen {
+                NavigationView {
+                    EmptyView()
+                        .navigationBarTitle(Text("Navigation Drawer"))
+                        .navigationBarItems(leading: Button(action: {
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+                                self.isDrawerOpen.toggle()
+                            }
+                        }) {
+                            Image(systemName: "sidebar.left")
+                        })
+                }
+                .navigationViewStyle(StackNavigationViewStyle())
+            }
+            /// Navigation Drawer part
+            NavigationDrawer(isOpen: self.isDrawerOpen)
+         /// Other behaviors
+        }.background(Color.white)
+        .onTapGesture {
+            if self.isDrawerOpen {
+                self.isDrawerOpen.toggle()
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+  static var previews: some View {
+    ContentView()
+  }
+}
+
+struct DrawerContent: View {
+    var body: some View {
+        Color.blue
+    }
+}
+
+struct NavigationDrawer: View {
+    private let width = UIScreen.main.bounds.width + 8
+    let isOpen: Bool
+    
+    var body: some View {
+        HStack {
+            DrawerContent()
+                .frame(width: self.width)
+              .offset(x: self.isOpen ? self.width/2 : self.width)
+                .animation(.default)
+            Spacer()
+        }
     }
 }
